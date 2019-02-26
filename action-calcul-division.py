d5e6f6ed3bfc03147c5d0989301f091a4f8facbd
Cédric Martin <cedmart1@gmail.com>
2019-02-26 09:30:12 +0100

    - Updated skill to platform 0.61

--- action-calcul-division.py
+++ action-calcul-division.py
@@ -3,6 +3,7 @@
 
 import ConfigParser
 from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
 import io
 import math
@@ -53,6 +54,8 @@ def action_wrapper(hermes, intentMessage, conf):
 
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
+    mqtt_opts = MqttOptions()
+    
+    with Hermes(mqtt_options=mqtt_opts) as h:
         h.subscribe_intent("dbddv01:getDivision", subscribe_intent_callback) \
 .start()
